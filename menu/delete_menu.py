0ba7f4db3e4d629545c3a6469b8c88845ff76ff1
shenchris <47095710+shenchris@users.noreply.github.com>
2020-06-14 17:37:46 +0800

    Add files via upload

---
+++ menu/delete_menu.py
@@ -0,0 +1,20 @@
+import pymongo
+import json
+import base64
+
+#connect to mongodb
+myclient = pymongo.MongoClient("mongodb://root:cnmc@140.131.149.39:465")
+mydb = myclient["menu"]
+mycol = mydb["menu"]
+
+#data
+name = "炒面"
+date = 20200614
+
+#filter
+myquery={ "name" : name, "date" : date }
+
+#delete
+mycol.delete_one(myquery)
+
+
---
+++ menu/query_menu.py
@@ -0,0 +1,21 @@
+import pymongo
+import json
+
+#connect to mongodb
+myclient = pymongo.MongoClient("mongodb://root:cnmc@140.131.149.39:465")
+mydb = myclient["menu"]
+mycol = mydb["menu"]
+
+#filter
+name = "炒面" 
+date = 20200614
+
+#query
+myquery = { "name" : name , "date" : date }
+mydoc = mycol.find(myquery)
+
+#save result in result.txt
+with open("result.txt", "w") as query_result:
+  for x in mydoc:
+    query_result.write(str(x).replace("\\n",''))
+
