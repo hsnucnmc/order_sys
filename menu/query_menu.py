45770bef51c4085de7cf1260c34d50809ca59f2f
hahabox0 <52237469+hahabox0@users.noreply.github.com>
2020-06-15 13:54:09 +0800

    query_menu_image
    三合一ㄐㄐ

--- menu/query_menu.py
+++ menu/query_menu.py
@@ -1,20 +1,19 @@
 import pymongo
 import json
+import base64
 
-# connect to mongodb
+#connect to mongodb
 myclient = pymongo.MongoClient("mongodb://root:cnmc@140.131.149.39:465")
 mydb = myclient["menu"]
 mycol = mydb["menu"]
 
-# filter
-name = "炒面"
-date = 20200614
-
-# query
-myquery = {"name": name, "date": date}
+myquery = { "name" : "炒面" }
 mydoc = mycol.find(myquery)
-
-# save result in result.txt
-with open("result.txt", "w") as query_result:
-    for x in mydoc:
-        query_result.write(str(x).replace("\\n", ''))
+init_db=""
+for x in mydoc:
+    init_db += str(x)
+database=r.split("\'image\':")[0]+r.split("\'image\': b\'")[1].split("\', \'")[1]
+print(database)
+image=base64.decodebytes(init_db.split("\'image\': b\'")[1].split("\', \'")[0].replace("\\n","").encode("UTF-8"))
+image_result = open('decode.jpg', 'wb')
+image_result.write(image)
